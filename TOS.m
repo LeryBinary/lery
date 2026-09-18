9e5c3b9c9a850e37e3fecb0326573e961c06b258
LeryBinary <akaricommunity133@gmail.com>
2026-09-18 15:21:10 +0200

    Add Terms of Service document for Lery bot

---
+++ TOS.m
@@ -0,0 +1,14 @@
+# Terms of Service
+
+**Effective Date:** [Date]
+
+## 1. Scope
+By using our Discord server and our bot **Lery**, you accept these Terms of Service.
+
+## 2. Code of Conduct
+* Treat all members with respect.
+* Spam, harassment, advertising, or sharing inappropriate content is strictly prohibited.
+* Follow the instructions of the team (**Binary**) and the moderators at all times.
+
+## 3. Disclaimer
+The Binary team assumes no liability for any damages resulting from the use of the server or the bot Lery. We reserve the right to modify or discontinue the service at any time.
